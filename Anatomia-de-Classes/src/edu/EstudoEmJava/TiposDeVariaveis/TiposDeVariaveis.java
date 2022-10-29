e8baa8ef9adc65590712a5bb06c9f141e5ddce80
pedrotmaa <pedro-benedet@hotmail.com>
2022-10-29 09:43:54 -0300

    Tipos de Variáveis

---
+++ Anatomia-de-Classes/src/edu/EstudoEmJava/TiposDeVariaveis/TiposDeVariaveis.java
@@ -0,0 +1,24 @@
+package edu.EstudoEmJava.TiposDeVariaveis;
+
+import java.sql.SQLInvalidAuthorizationSpecException;
+
+public class TiposDeVariaveis {
+
+    byte idade = 123;
+
+    short ano = 2022;
+
+    int cep = 21070333; //se começar com zero, talvez tenha que ser outro tipo.
+
+    long cpf = 98765432109L; //se começar com zero, talvez tenha que ser outro tipo.
+
+    // vairável long precisa terminar com a letra L para conseguir ser armazenada adequadamente.
+
+    float pi = 3.14F;
+    // variável float precisa terminar com a letra F para conseguir ser armazaenada adequadamente.
+
+    final double SALARIO = 1275.33;
+
+
+    
+}
