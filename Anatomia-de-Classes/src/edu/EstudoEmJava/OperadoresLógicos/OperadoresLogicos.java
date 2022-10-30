1bb582ce01ecaf48fe6205e3c249a6b566eba3b3
pedrotmaa <pedro-benedet@hotmail.com>
2022-10-30 11:32:42 -0300

    Operadores Lógicos

---
+++ Anatomia-de-Classes/src/edu/EstudoEmJava/OperadoresLógicos/OperadoresLogicos.java
@@ -0,0 +1,72 @@
+package edu.EstudoEmJava.OperadoresLógicos;
+
+public class OperadoresLogicos {
+    public static void main(String[] args){
+        //Operador de Igualdade "=":
+        String meuNome = "Pedro Guilherme";
+        int numero = 120;
+        double peso = 80.5;
+        char sexo = 'M';
+        Boolean doadorOrgao = false;
+        //Date dataNascimento = new Date(); <-- Criação de novo objeto usando classe.
+
+        //Operadores Aritméticos "+, -, *, /, %":
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicação = 20 * 7;
+        int divisão = 15 / 3;
+        int modulo = 18 % 3; //Utilizado para saber se o resultado é facionado lendo a divisão mostrando se tem resto ou não.
+        double resultado = (10 * 7) + (20 / 4);
+
+        //Operador Unário "Realizam o trabalho básico como incrementar, decrementar, inverter valores numéricos e booleanos":
+        int numero1 = 5;
+        numero1 = - numero1;
+        System.out.println(numero1);
+        numero1 = - numero1; // ou numero1 = numero1 * -1; para poder torna-lo positivo.
+        System.out.println(numero1);
+
+        numero1++; // abreviação da expressão: numero1 = numero1 + 1; usado em expressões de repetição.
+        System.out.println(numero1);
+        System.out.println( ++ numero1);
+
+        numero1--; // abreviação da expressão: numero1 = numero1 - 1; 
+        System.out.println(numero1);
+        System.out.println( -- numero1);
+
+        boolean variavel = true;
+        System.out.println(!variavel); // desta maneira torna-se FALSE, não alterando de fato o valor TRUE.
+        
+        variavel = !variavel; // desta maneira torna-se o conteúdo da variável de fato FALSE.
+        System.out.println(variavel);
+
+        //Operador Ternário, é como se fosse uma condição IF normal, porém, de uma forma em que toda a sua estrutura estará escrita numa única linha.
+        int a, b;
+        a = 5;
+        b = 5;
+        String resultado1 = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado1);
+
+        /*Exemplo de uma condicional  utilizando estrutura IF ELSE
+        if (a==b)
+            resultado1 = "verdadeiro";
+        else
+            resultado1 = "falso";
+        */
+
+        /*Operadores Relacionais:
+        ==, Igual
+        !=, Diferente de
+        >, Maior que
+        >=, Maior Igual
+        <, Menor que
+        <= Menor Igual
+        */
+
+        /*Operadores Lógicos
+        && Operador Lógico "E"
+        || Operador Lógico "OU"
+         */
+
+    }
+
+}
